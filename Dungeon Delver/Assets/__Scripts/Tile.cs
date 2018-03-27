b8b0447ad706479d83bb707dbc5dbab781e58e5c
sunzhaoyuan <sunzhao.cool@gmail.com>
2018-03-27 18:01:28 -0400

    finish character movement

---
+++ Dungeon Delver/Assets/__Scripts/Tile.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+	[Header ("Set Dynamically")]
+	public int x;
+	public int y;
+	public int tileNum;
+
+	public void SetTile (int eX, int eY, int eTileNum = -1)
+	{ //a
+		x = eX;
+		y = eY;
+		transform.localPosition = new Vector3 (x, y, 0);
+		gameObject.name = x.ToString ("D3") + "x" + y.ToString ("D3"); // b
+		if (eTileNum == -1) {
+			eTileNum = TileCamera.GET_MAP (x, y); // c
+		}
+		tileNum = eTileNum;
+		GetComponent<SpriteRenderer> ().sprite = TileCamera.SPRITES [tileNum]; //d
+	}
+}
